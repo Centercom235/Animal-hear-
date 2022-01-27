1c90fdad949adb90a4f76fa1ca351a35a82dcf9c
Centercom235 <79948838+Centercom235@users.noreply.github.com>
2022-01-26 20:10:46 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function startclassification(){
+function startAudio(){
     navigator.mediaDevices.getUserMedia({audio:true});
     classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/fjkAGnn0U/model.json", modelReady);
 }
@@ -20,3 +20,36 @@ if(error){
     document.getElementById("result_confidence").innerHTML="Accuracy: "+(results[0].confidence*100).toFixed(2)+" %";
     document.getElementById("result_label").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
     document.getElementById("result_confidence").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+
+    img=document.getElementById("animalImage");
+        
+       
+
+        if(results[0].label=="Barking - Dog"){
+            img.src="JoJo.jpg";
+            
+        }
+        else if(results[0].label=="Kaw - Kaw - Parrot"){
+           
+            img.src="Karen.webp";
+            
+        }
+        else if(results[0].label=="Meowing - Cat"){
+         
+           
+            img.src="pop.jpg";
+            
+        }
+        else if(results[0].label=="Ooga - Booga- Gorilla")
+        {
+            
+           
+            
+            img.src="Sir Phones alot.jpeg";
+        }
+        else{
+            img.src="https://t4.ftcdn.net/jpg/02/18/96/33/360_F_218963301_G23swMwicGyTNqFnR1dqnhb96HPskm5r.jpg";
+        }
+    }
+    
+
